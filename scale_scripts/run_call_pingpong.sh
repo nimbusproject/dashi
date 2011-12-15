8189b1f3077d6c29ec96acb9d55a839437f1895a
BuzzTroll <bresnaha@mcs.anl.gov>
2011-12-15 11:36:00 -0600

    tweaks  Please enter the commit message for your changes. Lines starting

--- scale_scripts/run_call_pingpong.sh
+++ scale_scripts/run_call_pingpong.sh
@@ -3,11 +3,14 @@
 pinger_host=$1
 ponger_host=$2
 amqp_host=$3
-out_dir="LONG_TESTcallpingpong".`date +%s`
+
+exchange=`uuidgen`
+
+out_dir="call_pingpong".`date +%s`
 mkdir $out_dir
-out_file=$out_dir/callpingpong.data
+out_file=$out_dir/pingpong.data
 l_conf_file=`pwd`/ping.yml
-r_conf_file=/tmp/ping.yml
+r_conf_file=/tmp/conf.yml
 py=/home/bresnaha/DASHITESTS/bin/python
 
 l_pgm_file=`pwd`/call_pingpong.py
@@ -28,14 +31,14 @@ for i in `seq $trials`
 do
     echo "running: $i"
 
-    cpu_file=$out_file.callpong.$i
+    cpu_file=$out_file.pong.$i
     ssh $ponger_host top -b -d 1 > $cpu_file&
     kill_pid1=$!
-    cpu_file=$out_file.callping.$i
+    cpu_file=$out_file.$i.$cnt.$sz.sendcpu
     ssh $pinger_host top -b -d 1 > $cpu_file&
     kill_pid2=$!
 
-    cmd_line_args="--server.amqp.host=$amqp_host"
+    cmd_line_args="--server.amqp.host=$amqp_host --dashi.exchange=$exchange"
 
     ssh $ponger_host $py $r_pgm_file --test.type=pong $cmd_line_args $r_conf_file &
     recv_pid=$!
@@ -43,11 +46,11 @@ do
     ssh $pinger_host $py $r_pgm_file --test.type=ping $cmd_line_args $r_conf_file >> $out_file
 
     echo "pinger finished, waiting for ponger"
+    wait $recv_pid
     echo "ponger finished"
-    kill $recv_pid
     kill $kill_pid1
     kill $kill_pid2
-    echo "waiting on kills"
+    echo "kill sent, wiating"
     wait
     sleep 2
 
