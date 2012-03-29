2695716897fe2e581e70f24703a157c11b0f6931
David LaBissoniere <labisso@gmail.com>
2012-03-29 20:52:09 +0200

    Update setup.py for first release

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
+import os
+import codecs
+
+VERSION = "0.1.0"
+
+if os.path.exists("README.rst"):
+    long_description = codecs.open('README.rst', "r", "utf-8").read()
+else:
+    long_description = "See http://github.com/nimbusproject/dashi"
+
 setupdict = {
     'name' : 'dashi',
-    'version' : '0.1',
+    'version' : VERSION,
     'description' : 'simple RPC layer on top of kombu',
+    'long_description' : long_description,
     'license' : 'Apache 2.0',
     'author' : 'Nimbus team',
     'author_email' : 'nimbus@mcs.anl.gov',
     'keywords': ['nimbus','amqp', 'kombu'],
     'classifiers' : [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
@@ -18,6 +29,8 @@ setupdict = {
     "Topic :: System :: Distributed Computing",
     "Topic :: System :: Networking",
     "Topic :: Software Development :: Libraries :: Python Modules"],
+    "url" : "http://github.com/nimbusproject/dashi",
+    "download_url" : "http://www.nimbusproject.org/downloads/dashi-%s.tar.gz" % VERSION,
 }
 
 from setuptools import setup, find_packages
