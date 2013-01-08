c7806aea482976af63a49281c7395c3496c91239
Patrick Armstrong <patricka@uvic.ca>
2013-01-08 11:39:10 -0800

    Switch back to older kombu for now

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setupdict = {
     "download_url" : "http://www.nimbusproject.org/downloads/dashi-%s.tar.gz" % VERSION,
 }
 
-install_requires = ['kombu>=2.5', 'pyyaml']
+install_requires = ['kombu>=2.1.2,<2.5.0', 'pyyaml']
 tests_require = ["nose", "mock"]
 
 from setuptools import setup, find_packages
